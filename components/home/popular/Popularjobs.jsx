e13bf635f83a14f6eeb23a85abec3fd5c46935a3
devAhmed94 <ahmedali26002844@gmail.com>
2023-08-13 03:12:27 +0300

    1

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
-import { View, Text } from 'react-native'
-
+import { View, Text, TouchableOpacity, FlatList, ActivityIndicator } from 'react-native'
+import { useState } from 'react'
 import styles from './popularjobs.style'
+import { useRouter } from 'expo-router'
+import { COLORS, SIZES } from '../../../constants'
+import PopularJobCard from '../../common/cards/popular/PopularJobCard';
 
 const Popularjobs = () => {
+  const router = useRouter();
+
   return (
-    <View>
-      <Text>Popularjobs</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+      <Text style={styles.headerTitle}>
+        Popular Jobs
+      </Text>
+      <TouchableOpacity>
+        <Text style={styles.headerBtn}>
+        Show All
+        </Text>
+      </TouchableOpacity>
+      </View>
     </View>
   )
 }
--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -5,8 +5,11 @@ import styles from './welcome.style'
 import { useRouter } from 'expo-router'
 import { icons, SIZES } from '../../../constants'
 
+const jobTypes=["Full-time", "Part-time", "Contractor"]
+
 const Welcome = () => {
   const router = useRouter();
+  const [activeJonType, setActiveJobType]=useState('Full-time')
   return (
     <View>
       <View style={styles.container}>
@@ -19,13 +22,39 @@ const Welcome = () => {
       </View>
       <View style={styles.searchContainer}>
         <View style={styles.searchWrapper}>
-          <TextInput 
-          style={styles.searchInput}
-          value=''
-          onChange={()=>{}}
-          placeholder='What are you looking for'
+          <TextInput
+            style={styles.searchInput}
+            value=''
+            onChange={() => { }}
+            placeholder='What are you looking for'
           />
         </View>
+        <TouchableOpacity style={styles.searchBtn} onPress={() => { }}>
+          <Image source={icons.search} resizeMode='contain' style={styles.searchBtnImage} />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.tabsContainer}>
+            <FlatList
+            data={jobTypes}
+            renderItem={({ item })=>(
+              <TouchableOpacity
+              style={styles.tab(activeJonType, item)}
+              onPress={()=>{
+                setActiveJobType(item);
+                router.push('/search/${item}')
+              }}
+              >
+                <Text style={styles.tabText(activeJonType, item)}>
+                  {item}
+                </Text>
+              </TouchableOpacity>
+            )}
+            keyExtractor={item=>item}
+            contentContainerStyle={{
+              columnGap:SIZES.small
+            }}
+            horizontal
+            />
       </View>
     </View>
   )
